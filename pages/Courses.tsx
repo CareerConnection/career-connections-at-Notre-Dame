fd02c2f5b8dd4af41331a4b4ae02654b0b8ab461
gen <genesisargueta@genesiss-mbp.dhcp.nd.edu>
2024-04-22 20:54:34 -0400

    user able to navigate to course description when clicked on in pathways

--- pages/Courses.tsx
+++ pages/Courses.tsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 import Parse from "parse";
 import { APPLICATION_ID, JAVASCRIPT_KEY, SERVER_URL } from "../environment";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 // Initialize Parse SDK
 Parse.initialize(APPLICATION_ID, JAVASCRIPT_KEY);
 Parse.serverURL = SERVER_URL;
 
-// Function to fetch all objects of a specific class
 const getAllObjects = async (className) => {
   try {
     const query = new Parse.Query(className);
@@ -20,6 +20,8 @@ const getAllObjects = async (className) => {
 };
 
 const Dashboard: React.FC = () => {
+  const router = useRouter();
+  const { course } = router.query;
   const [courses, setCourses] = useState([]);
   const [selectedCourse, setSelectedCourse] = useState(null);
   const [additionalInfo, setAdditionalInfo] = useState(null);
@@ -32,10 +34,20 @@ const Dashboard: React.FC = () => {
     const fetchData = async () => {
       const courses = await getAllObjects("Class");
       setCourses(courses);
-    };
 
+      if (course) {
+        const selectedCourse = courses.find(
+          (c) =>
+            c.class.toLowerCase() ===
+            decodeURIComponent(course as string).toLowerCase()
+        );
+        if (selectedCourse) {
+          handleCourseClick(selectedCourse);
+        }
+      }
+    };
     fetchData();
-  }, []);
+  }, [course]);
 
   const handleCourseClick = async (course) => {
     setSelectedCourse(course);
@@ -63,24 +75,13 @@ const Dashboard: React.FC = () => {
   let finalCourses = [...sortedCourses];
 
   if (filterByProfessor) {
-    finalCourses = finalCourses.filter(
-      (course) =>
-        course.professors.some(
-          (professor) =>
-            professor.toLowerCase() === filterByProfessor.toLowerCase()
-        )
+    finalCourses = finalCourses.filter((course) =>
+      course.professors.some(
+        (professor) =>
+          professor.toLowerCase() === filterByProfessor.toLowerCase()
+      )
     );
   }
-  // const handleAddToCart = (course) => {
-  //   const existingCartItemIndex = cart.findIndex((item) => item.objectId === course.objectId);
-  //   if (existingCartItemIndex !== -1) {
-  //     const updatedCart = [...cart];
-  //     updatedCart[existingCartItemIndex].quantity += 1;
-  //     setCart(updatedCart);
-  //   } else {
-  //     setCart([...cart, { ...course, quantity: 1 }]);
-  //   }
-  // };
 
   return (
     <>
@@ -88,7 +89,12 @@ const Dashboard: React.FC = () => {
         <div className="w-full mt-6 max-w-5xl">
           <div className="text-4xl">Courses</div>
           <div className="flex mt-4">
-            <div className="w-1/2" style={{ transform: selectedCourse ? "translateX(0)" : "translateX(0%)" }}>
+            <div
+              className="w-1/2"
+              style={{
+                transform: selectedCourse ? "translateX(0)" : "translateX(0%)",
+              }}
+            >
               <div className="flex items-center">
                 <input
                   type="text"
@@ -97,14 +103,6 @@ const Dashboard: React.FC = () => {
                   onChange={(e) => setSearchQuery(e.target.value)}
                   className="border border-gray-300 p-2 rounded-md mr-2"
                 />
-                {/* <select
-                  value={sortBy}
-                  onChange={(e) => setSortBy(e.target.value)}
-                  className="border border-gray-300 p-2 rounded-md mr-2"
-                >
-                  <option value="">Sort by...</option>
-                  <option value="rating">Rating</option>
-                </select> */}
                 <select
                   value={filterByProfessor}
                   onChange={(e) => setFilterByProfessor(e.target.value)}
@@ -120,28 +118,25 @@ const Dashboard: React.FC = () => {
                   )}
                 </select>
               </div>
-              {finalCourses.sort((a, b) => a.class.localeCompare(b.class))
-              .map((course, index) => (
-                <div
-                  key={index}
-                  className="border border-gray-300 p-4 mt-4 rounded-md cursor-pointer hover:bg-gray-200 transition-all duration-200 ease-out"
-                  onClick={() => handleCourseClick(course)}
-                >
-                  <h3 className="text-xl font-bold">{course.class}</h3>
-                  <p>Professor: {course.professors.join(", ")}</p>
-                  <p>ID: {course.classID}</p>
-                  {/* <button
-                  <button
-                    onClick={() => handleAddToCart(course)}
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              {finalCourses
+                .sort((a, b) => a.class.localeCompare(b.class))
+                .map((course, index) => (
+                  <div
+                    key={index}
+                    className="border border-gray-300 p-4 mt-4 rounded-md cursor-pointer hover:bg-gray-200 transition-all duration-200 ease-out"
+                    onClick={() => handleCourseClick(course)}
                   >
-                    Add to Cart
-                  </button> */}
-                </div>
-              ))}
+                    <h3 className="text-xl font-bold">{course.class}</h3>
+                    <p>Professor: {course.professors.join(", ")}</p>
+                    <p>ID: {course.classID}</p>
+                  </div>
+                ))}
             </div>
             {selectedCourse && additionalInfo && (
-              <div className="w-1/2 pl-4 pr-4" style={{ transform: "translateX(10%)" }}>
+              <div
+                className="w-1/2 pl-4 pr-4"
+                style={{ transform: "translateX(10%)" }}
+              >
                 <div className="flex flex-col max-h-[80vh] sticky top-6 border border-gray-300 p-4 gap-2 rounded-md overflow-auto">
                   <h3 className="text-xl font-bold">{selectedCourse.class}</h3>
                   <Link
@@ -160,9 +155,18 @@ const Dashboard: React.FC = () => {
                       Past Syllabus
                     </Link>
                   )}
-                  <p><a className="font-bold">Difficulty:</a> {additionalInfo.difficulties}/5</p>
-                  <p><a className="font-bold">Description:</a> {additionalInfo.description}</p>
-                  <p><a className="font-bold">Summarized Review:</a> {additionalInfo.reviews}</p>
+                  <p>
+                    <a className="font-bold">Difficulty:</a>{" "}
+                    {additionalInfo.difficulties}/5
+                  </p>
+                  <p>
+                    <a className="font-bold">Description:</a>{" "}
+                    {additionalInfo.description}
+                  </p>
+                  <p>
+                    <a className="font-bold">Summarized Review:</a>{" "}
+                    {additionalInfo.reviews}
+                  </p>
                 </div>
               </div>
             )}
@@ -174,4 +178,3 @@ const Dashboard: React.FC = () => {
 };
 
 export default Dashboard;
-
